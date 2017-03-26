b5b6cd746fbf4444b93642c9f24253c816420c3e
Thiago Duarte <thiago-duarte@outlook.com>
2017-03-26 19:45:30 +0000

    Alteração no HEADER

--- api/config/express.js
+++ api/config/express.js
@@ -33,8 +33,9 @@ module.exports = function()
 	
 	app.use(function(req, res, next){
 	  res.header("Access-Control-Allow-Origin", "*");
-      res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+	  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+      //res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 	  next();   
 	});
 
