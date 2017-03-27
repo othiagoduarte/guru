42c92a4532cf96ce7ebd186951c9fe74c6247e9e
Thiago Duarte <thiago-duarte@outlook.com>
2017-03-26 21:11:55 -0300

    Alualização

--- web/src/app/app.js
+++ web/src/app/app.js
@@ -21,6 +21,6 @@ var app = angular.module('BlurAdmin', [
 app.constant("authorization", {tipo:"JWT", token:""});
 app.constant("PERFIL", "ALUNO");
 app.run(function ($rootScope,$state,authorization, $window) {
-    $window.sessionStorage.token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjU4ZDdmODY5NjU0ZDY5NjFmZWY4OTQwNSJ9.QgL9BSI3NCU5Bl74L0zuu-cIbO17gK5pwhlWS1Lq-1I";
+   // $window.sessionStorage.token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjU4ZDdmODY5NjU0ZDY5NjFmZWY4OTQwNSJ9.QgL9BSI3NCU5Bl74L0zuu-cIbO17gK5pwhlWS1Lq-1I";
     authorization.token = $window.sessionStorage.token;
 });
--- web/src/app/appAuth.js
+++ web/src/app/appAuth.js
@@ -1,7 +1,34 @@
 'use strict';
 var app = angular.module('Auth',[]);
 
-app.controller('myCtrl', function($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
+app.controller('loginCtrl', function($scope,$http,$window) {
+    
+    $window.sessionStorage.clear();
+    
+    $scope.login = login;
+    $scope.data = {};
+    $scope.data.password  = null;
+    $scope.data.email ="";
+    
+    console.log("Controller ready");
+
+    function login(){
+ 
+        if($scope.data && $scope.data.password != "" && $scope.data.email !="" ){
+            
+            $http.post("https://guru-othiagoduarte.c9users.io/login", $scope.data)
+            .then(function(res){
+                $window.sessionStorage.token = res.data.token ;
+                $window.sessionStorage.perfil = res.data.user.tipo;
+                window.location.href ="/#/" + res.data.user.tipo ;
+            })
+            .catch(function(res){
+                if(res.data.retorno){
+                    alert(res.data.retorno);
+                }
+                $scope.data.password  =null;
+            });
+
+        }
+    }
 });
