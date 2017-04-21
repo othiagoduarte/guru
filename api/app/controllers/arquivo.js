7eb08eb497fd945320d4f5e0b9e88da57b16572f
Thiago Duarte <thiago-duarte@outlook.com>
2017-04-21 18:29:34 -0300

    teste de publicação

--- api/app/controllers/arquivo.js
+++ api/app/controllers/arquivo.js
@@ -1,5 +1,5 @@
-var multer  = require('multer')
-var upload = multer({ dest: 'uploads/' })
+//var multer  = require('multer')
+var upload = {}// multer({ dest: 'uploads/' })
 
 module.exports = function(app)
 {
