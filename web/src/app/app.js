c044f7b37518d343959c956c9dfbd8ec6d1061cc
Thiago Duarte <thiago-duarte@outlook.com>
2017-04-21 22:57:13 -0300

    Deploy no heroku

--- web/src/app/app.js
+++ web/src/app/app.js
@@ -23,12 +23,10 @@ var app = angular.module('BlurAdmin', [
 app.constant("authorization", {tipo:"JWT", token:""});
 app.constant("$USERDATA",JSON.parse(window.sessionStorage.userData));
 app.constant("PERFIL", "");
-
-app.constant("$URLAPI", "https://guru-api-othiagoduarte.c9users.io/");
+app.constant("$URLAPI", "https://guru-web.herokuapp.com/");
 /*
-app.constant("$URLAPI", "https://othiagoduarte.herokuapp.com/");
+app.constant("$URLAPI", "https://guru-api-othiagoduarte.c9users.io/");
 */
-
 /*
 app.constant("$URLAPI", "http://localhost:3008/");
 */
