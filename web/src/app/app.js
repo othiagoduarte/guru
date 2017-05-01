cd2642e3358a8066577c5970e2a7e791aa4d6973
Thiago Duarte <thiago-duarte@outlook.com>
2017-05-01 17:39:24 -0300

    Login - correção de bug

--- web/src/app/app.js
+++ web/src/app/app.js
@@ -4,7 +4,9 @@ var app = angular.module('BlurAdmin',modulos())
 .constant("$USERDATA",JSON.parse(window.sessionStorage.userData))
 .constant("PERFIL", "")
 //.constant("$URLAPI", "https://guru-web.herokuapp.com/");
+
 .constant("$URLAPI", "http://localhost:3008/");
+/**/
 app.run(function ($rootScope,$state,authorization, $window,PERFIL) {
     authorization.token = $window.sessionStorage.token;
     PERFIL = $window.sessionStorage.perfil;
