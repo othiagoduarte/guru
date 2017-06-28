85bfeceaa747a43156a82cefea61ca8d70c2dfbc
Thiago Duarte <thiago-duarte@outlook.com>
2017-06-28 00:05:13 -0300

    Refactor

--- web/src/app/app.js
+++ web/src/app/app.js
@@ -3,8 +3,10 @@ var app = angular.module('BlurAdmin',modulos())
 .constant("authorization", {tipo:"JWT", token:""})
 .constant("$USERDATA",JSON.parse(window.sessionStorage.userData))
 .constant("PERFIL", "")
-//.constant("$URLAPI", "https://guru-web.herokuapp.com/");
+.constant("$URLAPI", "https://guru-web.herokuapp.com/");
+/*
 .constant("$URLAPI", "http://localhost:3008/");
+*/
 app.run(function ($rootScope,$state,authorization, $window,PERFIL) {
     authorization.token = $window.sessionStorage.token;
     PERFIL = $window.sessionStorage.perfil;
