e42c1f66f0c44bae10311a19a721ec0f73de21ac
Thiago Duarte <thiago-duarte@outlook.com>
2017-09-23 22:14:02 -0300

    Recuperação de senha

--- api/app/lib/emailRecuperacaoSenha.js
+++ api/app/lib/emailRecuperacaoSenha.js
@@ -7,7 +7,8 @@ module.exports = function(app)
         enviar: function(email, senha){
             console.log(email, senha);
             var message = "<h3>Sua senha foi recuperada!<h3>" +
-                          "<p>Senha : <b>{{senha}}</b></p>"; 
+                          "<p>Senha : <b>{{senha}}</b></p> "+ 
+                          "<p>Click <a href=\"http://guru-web.herokuapp.com/\"> aqui</a> para acessar!</p>"; 
             //fs.createReadStream('./app/lib/email-template/recuperacao-senha.html');
             var subject = "[GURU] - Recuperação de senha!";
             var to = email;
